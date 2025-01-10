a619f8196c29a6ebbb156dce75ecd6297b5576d0
aki05162525 <akihiro05162525@gmail.com>
2025-01-10 23:05:58 +0900

    Updates

--- src/utils.js
+++
@@ -1,8 +0,0 @@
-// export const getImageUrl = (path) => {
-//   return new URL(`/assets/${path}`, import.meta.url).href;
-// };
-//なんで上のではうまくいかなかったのか？？？
-
-export const getImageUrl = (path) => {
-  return `${import.meta.env.BASE_URL}assets/${path}`; // Viteの base を使用
-};
